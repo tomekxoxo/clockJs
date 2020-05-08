dc3039af3cea6219827e19aceca8614a1308054a
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-05-08 14:07:06 +0200

    simpler js mechanism

---
+++ app.js
@@ -0,0 +1,27 @@
+setSeconds();
+setMinutes();
+setHours();
+  setInterval(setSeconds, 1000);
+  function setSeconds() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() * 6;
+    const secondHand = document.querySelector(".clock__hand--seconds");
+    secondHand.style.setProperty("--rotation-seconds", secondsRatio);
+  }
+
+  setInterval(setMinutes, 1000);
+  function setMinutes() {
+    const currentDate = new Date();
+    const minuteRatio = currentDate.getMinutes() * 6;
+    const minuteHand = document.querySelector(".clock__hand--minute");
+    minuteHand.style.setProperty("--rotation-minutes", minuteRatio);
+  }
+
+  setInterval(setHours,1000);
+  function setHours() {
+    const currentDate = new Date();
+    const hoursRatio = currentDate.getHours() % 12;
+    const hoursHand = document.querySelector(".clock__hand--hour");
+    hoursHand.style.setProperty("--rotation-hours", hoursRatio * 30);
+  }
+
