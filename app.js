6d7f23386328cb88428ccaa41306cff9a1f75387
tomasz kasprowicz <tomaszkasprowicz24@gmail.com>
2020-05-08 16:29:14 +0200

    upgraded clock mechanism, added arrow effects

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ setSeconds();
 setMinutes();
 setHours();
 setDate();
+
 function setDate(){
   const currentDate = new Date();
-  const date = currentDate.getUTCDate()
+  const date = currentDate.getUTCDate();
   document.querySelector('.clock__date').innerText = date
 }
-setInterval(setSeconds, 1000);
+setInterval(setSeconds,1000);
 function setSeconds() {
   const currentDate = new Date();
   const secondsRatio = currentDate.getSeconds() * 6;
@@ -19,14 +20,16 @@ setInterval(setMinutes, 1000);
 function setMinutes() {
   const currentDate = new Date();
   const minuteRatio = currentDate.getMinutes() * 6;
+  const seconds = currentDate.getSeconds()
   const minuteHand = document.querySelector(".clock__hand--minute");
-  minuteHand.style.setProperty("--rotation-minutes", minuteRatio);
+  minuteHand.style.setProperty("--rotation-minutes", minuteRatio + seconds * 0.1);
 }
 
 setInterval(setHours, 1000);
 function setHours() {
   const currentDate = new Date();
   const hoursRatio = currentDate.getHours() % 12;
+  const minutes  = currentDate.getMinutes()
   const hoursHand = document.querySelector(".clock__hand--hour");
-  hoursHand.style.setProperty("--rotation-hours", hoursRatio * 30);
+  hoursHand.style.setProperty("--rotation-hours", hoursRatio * 30 + minutes * 0.5);
 }
